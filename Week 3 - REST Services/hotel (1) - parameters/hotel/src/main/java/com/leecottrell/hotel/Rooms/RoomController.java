df5dcc6c32a3a9e34a82fb69affe16a7fbff9222
Lee Cottrell <44240965+leecottrellPTC@users.noreply.github.com>
2022-10-31 13:45:31 -0400

    Room Controller Update
    I finished the 4 HTTP verbs of POST, GET, PUT, DELETE. I still need to validate the data on the server end.

--- Week 3 - REST Services/hotel (1) - parameters/hotel/src/main/java/com/leecottrell/hotel/Rooms/RoomController.java
+++ Week 3 - REST Services/hotel (1) - parameters/hotel/src/main/java/com/leecottrell/hotel/Rooms/RoomController.java
@@ -3,6 +3,9 @@ package com.leecottrell.hotel.Rooms;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.util.*;
 
 @RestController
@@ -21,31 +24,68 @@ public class RoomController {
     }    
 
     @RequestMapping(value="/rooms", method=RequestMethod.GET)
-    public ResponseEntity<List<Room>> getReservations(){
-        Collection<Room> results = reservations.values();
-        List response = new ArrayList<>(results);
+    public ResponseEntity<List<Room>> 
+        getReservations(@RequestParam(value="roomnum", defaultValue="0")int roomnum){
+       // Collection<Room> results = reservations.values();
+        List<Room> response = new ArrayList<>();
 
+        switch(roomnum){
+            case 0://all reservations in the liswt
+                response = new ArrayList<>(reservations.values());
+                break;
+            default://some room number passed in
+                Room found = reservations.get(roomnum);
+                if(found == null){
+                    response.add(new Room(roomnum, "Room num is empty"));
+                    return new ResponseEntity<List<Room>>(response,HttpStatus.NOT_FOUND);
+                }
+                else{
+                    response.add(reservations.get(roomnum));
+                }
+                break;
+        }//end of switch
         return new ResponseEntity<List<Room>>(response, HttpStatus.OK);
+        //return new ResponseEntity<List<Room>>(response, HttpStatus.OK);
     }
 
     @RequestMapping(value="/rooms", method=RequestMethod.POST)
-    public ResponseEntity<Room> postReservation(){
-        Room response = new Room(0, "empty");
+    public ResponseEntity<Room> postReservation(@RequestBody String reservation)
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        Room response = new Room(0, "Error in reservation");
+        try{
+            response = mapper.readValue(reservation, Room.class);
+            reservations.put(response.getRoomNum(), response);
+        }
+        catch(Exception ex){
+            response.setGuest(ex.toString());//send error back for me to fix
+            //later clean this up
+        }
 
-        return new ResponseEntity<Room>(response, HttpStatus.NOT_IMPLEMENTED);
+        return new ResponseEntity<Room>(response, HttpStatus.OK);
     }
 
     @RequestMapping(value="/rooms", method=RequestMethod.PUT)
-    public ResponseEntity<Room> putReservation(){
+    public ResponseEntity<Room> putReservation(@RequestParam(value="roomnum") int roomnum,
+        @RequestParam(value="addon") String addon){
         Room response = new Room(0, "empty");
 
-        return new ResponseEntity<Room>(response, HttpStatus.NOT_IMPLEMENTED);
+        String origGuest = reservations.get(roomnum).getGuest();
+        reservations.get(roomnum).setGuest(origGuest + " and " + addon);
+        response = reservations.get(roomnum);
+
+        return new ResponseEntity<Room>(response, HttpStatus.OK);
     }
 
     @RequestMapping(value="/rooms", method=RequestMethod.DELETE)
-    public ResponseEntity<Room> deleteReservation(){
+    public ResponseEntity<Room> 
+        deleteReservation(@RequestParam(value="roomnum") int roomnum){
         Room response = new Room(0, "empty");
 
-        return new ResponseEntity<Room>(response, HttpStatus.NOT_IMPLEMENTED);
+        reservations.remove(roomnum);
+        response.setRoomNum(roomnum);
+        response.setGuest("deleted");
+
+        return new ResponseEntity<Room>(response, HttpStatus.OK);
     }
 }
